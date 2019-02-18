41e65c7db146dbba08b765e982eb36845f18abe7
Justin Sitter <justin@jaytria.com>
2019-02-17 16:46:10 -0800

    Add App

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+class App extends Component {
+  render(){
+    return (
+      <section >
+       <p>Hello App!</p>
+      </section>
+    )
+  }
+}
+
+export default App;
